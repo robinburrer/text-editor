faaad469bf5254ccced6e1c0c70ed44d505bbcaf
Robin Burrer <robin.burrer@ptwdosimetry.com>
2024-01-10 15:25:22 +0100

    initial commit

---
+++ src/components/LinkInput.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+interface Props {
+  title: string
+}
+
+const props = defineProps<Props>();
+
+</script>
+
+<template>
+  <div class="link-area">
+    <label>{{ title }}</label>
+    <input type="text" />
+  </div>
+</template>
+
+<style scoped>
+
+label {
+  min-width: 80px;
+}
+.link-area {
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+  margin: 10px 0;
+}
+</style>
---
+++ src/components/Text-Editor.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import LinkInput from './LinkInput.vue';
+
+const addLink = () => {
+  console.log('add link')
+}
+</script>
+
+<template>
+  <div class="editor">
+
+
+  <h1>texteditor PT</h1>
+
+  <textarea> </textarea>
+  <hr>
+  <div class="link-area">
+    <LinkInput title="Link Name:" />
+    <LinkInput title="URL:" />
+  </div>
+
+  <button @click="addLink">Add Link</button>
+</div>
+</template>
+
+<style scoped>
+
+  h1 {
+    margin-bottom: 20px;
+    width: 100%;
+    text-align: center;
+  }
+  .editor {
+    width: 300px;
+    /* background-color: red; */
+  }
+
+
+  textarea {
+    width: 300px;
+    height: 200pX;
+  }
+
+
+</style>
